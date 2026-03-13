cc37c055ba727f75e9a86f49ccad469498772090
kmc77 <k_minchoi@naver.com>
2026-03-13 16:15:40 +0900

    fix: FINERACT-1971 outstanding principal 참조 로직 정상화

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/reaging/LoanReAgingServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/reaging/LoanReAgingServiceImpl.java
@@ -164,7 +164,8 @@ public class LoanReAgingServiceImpl {
         // [FINERACT-1971 버그 상황 완벽 재현]
         // getLoanSummary()는 Loan.java에 정의된 Getter입니다.
         // getTotalPrincipal()은 상환액과 상관없이 '최초 대출 총액'을 가져오는 메서드입니다.
-        BigDecimal txPrincipalAmount = loan.getApprovedPrincipal();
+        // BigDecimal txPrincipalAmount = loan.getApprovedPrincipal();
+        BigDecimal txPrincipalAmount = loan.getTotalPrincipalOutstandingUntil(transactionDate).getAmount();
 
         return new LoanTransaction(loan, loan.getOffice(), LoanTransactionType.REAGE.getValue(), transactionDate, txPrincipalAmount,
                 txPrincipalAmount, ZERO, ZERO, ZERO, null, false, null, txExternalId);
